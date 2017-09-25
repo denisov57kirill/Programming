4ced383e16624c0073665cdebaf81b08ce88a16a
denisov57kirill <denisovkirill57@yandex.ru>
2017-09-25 21:13:55 +0300

    homework

---
+++ homework1.py
@@ -0,0 +1,14 @@
+print('Введите a')
+a = int(input())
+print('Введите b')
+b = int(input())
+print('Введите c')
+c = int(input())
+if a+b==c:
+    print('Свойство 1 верно')
+else:
+    print('Свойство 1 неверно')
+if a*c+b==0:
+    print('Свойство 2 верно')
+else:
+    print('Свойство 2 неверно')
